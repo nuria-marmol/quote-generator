e032aecb644c3094587d7cc97ed581f437506508
nuria-marmol <numarbru@gmail.com>
2023-06-14 16:52:32 +0200

    structure and code

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<script setup>
+  import QuoteCard from './components/QuoteCard.vue'
+
+  import { ref, onMounted } from 'vue'
+
+  const quote = ref({})
+
+  function getQuote() { 	
+    fetch("https://quote-garden.onrender.com/api/v3/quotes/random")
+      .then((response) => response.json())
+      .then((json) => (quote.value = json.data[0]))
+  }
+
+  onMounted(async () => {
+    await getQuote()
+  })
+</script>
+
+<template>
+  <QuoteCard
+    :quoteText="quote.quoteText"
+    :quoteAuthor="quote.quoteAuthor"
+    @onClickDoThis="getQuote"
+  />
+</template>
+
+<style scoped></style>
---
+++ src/components/QuoteCard.vue
@@ -0,0 +1,16 @@
+<script setup>
+    defineProps({
+        quoteText: String,
+        quoteAuthor: String
+    })
+</script>
+
+<template>
+  <div>
+    <p>"{{ quoteText }}"</p>
+    <p>{{ quoteAuthor }}</p>
+    <button type="button" @click="$emit('onClickDoThis')">New quote</button>
+  </div>
+</template>
+
+<style scoped></style>
